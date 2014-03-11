794f24f8ef41de952d92140be8d413ddfa67fe55
Hack Reactor <hackreactor@HR-A4.local>
2014-03-11 12:43:16 -0700

    have room list

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,11 +3,12 @@
 var myName = window.location.search.substring(10);
 var myRoom = "4chan";
 var lastMessageTime = (new Date(0)).toJSON();
+var rooms = {};
 
 $(document).ready(function () {
   var msgObj = {};
 
-  var sendMsg = function (message) {
+  var postMSG = function (message) {
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: "POST",
@@ -40,7 +41,7 @@ $(document).ready(function () {
     lastMessageTime = results[0].createdAt;
   };
 
-  var postRetrieval = function(){
+  var getMessages = function(){
     $.ajax({
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
@@ -62,6 +63,38 @@ $(document).ready(function () {
     });
   };
 
+  // get active rooms
+  var getActiveRooms = function () {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: {
+        order: '-createdAt',
+        limit: 500,
+        where: JSON.stringify({
+          createdAt: { $gt: {"__type": "Date", iso: lastMessageTime}}
+        })
+      },
+      success: function (data) {
+        if(data.results.length > 0){
+          roomFormat(data.results);
+        }
+      },
+      error: function (data) {
+        console.log('active rooms not retrieved');
+      }
+    });
+  };
+
+  var roomFormat = function (results) {
+    for (var i = 0; i < results.length; i++) {
+      rooms[results[i].roomname] = true;
+    }
+    for (var roomname in rooms) {
+      $('<span></span>').text(roomname).appendTo($('div.activeRooms'));
+    }
+  };
+
   // sending messages
   $('button.send').on('click', function (e) {
     var text = $('.draft').val();
@@ -69,7 +102,7 @@ $(document).ready(function () {
     msgObj.username = myName;
     msgObj.roomname = myRoom;
 
-    sendMsg(msgObj);
+    postMSG(msgObj);
   });
 
   // retrieving username from input box
@@ -83,46 +116,15 @@ $(document).ready(function () {
     $("h2.room").text(myRoom);
     $("div.messages").children().remove();
     lastMessageTime = (new Date(0)).toJSON();
-    postRetrieval();
+    getMessages();
 
   });
- postRetrieval();
- setInterval(postRetrieval, 3000);
+
+ getActiveRooms();
+ getMessages();
+
+ setInterval(function () {
+   getActiveRooms();
+   getMessages();
+ }, 3000);
 });
-
-/*
-var message = {
-  'username': 'gregLuke',
-  'text': 'hello',
-  'roomname': '4chan'
-};
-
-
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent', data);
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
-
-
-$.ajax({
-  url: 'https://api.parse.com/1/classes/chatterbox/UEZw3Rll2k',
-  type: 'GET',
-
-  success: function (data) {
-    console.log("unparsed: ", data);
-      },
-  error: function (data) {
-    console.log('not retrieved');
-  }
-});
-*/
