82ca84954159fcb90d6d8af1fa20ceb00cef8cc7
lramsey <lramsey177@gmail.com>
2014-03-12 08:12:55 -0700

    pre-paring work

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,50 @@
 // YOUR CODE HERE:
 
+$(document).ready(function(){
+   
+   var sendPost = function(message){
+      $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function(data) {
+            console.log('chatterbox: Message sent');
+        },
+        error: function(data) {
+            console.error('chatterbox: Failed to send message');
+        }
+    });
+  };
+  
+  var getPosts = function(){
+    $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'GET',
+        data: JSON.parse(data),
+        contentType: 'application/json',
+        success: function(data) {
+            console.log('chatterbox: Messages received');
+        },
+        error: function(data) {
+            console.error('chatterbox: Failed to receive messages');
+        }
+    });
+  }
+
+});
+
+/* $.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+}); */
